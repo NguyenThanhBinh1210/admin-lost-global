cb48165755fb03d1422704c181794975a5b91bd7
catalina5 <thanhbinh12369yt@gmail.com>
2023-11-14 14:06:50 +0700

    binh

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,18 +28,16 @@ const Header = () => {
     <div
       ref={modalRef}
       id='drawer-navigation'
-      className={`${
-        showMenu ? 'mobile:translate-x-[0] ' : 'mobile:translate-x-[-100%] '
-      } dark:bg-gray-700 mobile:fixed non-scroll dark:border-none border-r border-gray-300 top-0 sticky left-0 z-40 h-screen p-4  transition-all  bg-white w-60 mobile:w-[240px]`}
+      className={`${showMenu ? 'mobile:translate-x-[0] ' : 'mobile:translate-x-[-100%] '
+        } dark:bg-gray-700 mobile:fixed non-scroll dark:border-none border-r border-gray-300 top-0 sticky left-0 z-40 h-screen p-4  transition-all  bg-white w-60 mobile:w-[240px]`}
       tabIndex={-1}
       aria-labelledby='drawer-navigation-label'
     >
       <div
         id='drawer-navigation-label'
         style={{ textAlign: 'center' }}
-        className={` ${
-          showMenu ? 'hidden' : ''
-        } text-blue-400 flex justify-between items-center text-base font-semibold uppercase dark:text-gray-400 `}
+        className={` ${showMenu ? 'hidden' : ''
+          } text-blue-400 flex justify-between items-center text-base font-semibold uppercase dark:text-gray-400 `}
       >
         {profile?.isAdmin && <h2 style={{ margin: 'auto' }}>Admin</h2>}
         {profile?.isStaff && <h2 style={{ margin: 'auto' }}>Nhân viên</h2>}
@@ -159,14 +157,14 @@ const Header = () => {
                   <button>Khách hàng</button>
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   to='/chat'
                   className='flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-4 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700'
                 >
                   <button>Chat</button>
                 </Link>
-              </li>
+              </li> */}
             </>
           )}
         </ul>
